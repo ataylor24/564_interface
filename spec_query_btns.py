7f49630b8af69f4fe9b6d115af5814f885d9480c
Derek Wautlet <dhwaultet@gmail.com>
2020-07-25 17:31:11 -0500

    Added a special query for searching for State Data

--- spec_query_btns.py
+++ spec_query_btns.py
@@ -2,6 +2,18 @@ from tkinter import *
 import pymysql
 import credentials
 
+def dropdown_choices(attribute):
+    connection = pymysql.connect(credentials.host,credentials.username,credentials.password,credentials.db_name,
+    cursorclass=pymysql.cursors.DictCursor)
+    with connection.cursor() as cursor:
+        sql = "SELECT distinct %s FROM civilian" % (attribute)
+        #sql = "SELECT distinct state_name FROM state ORDER BY state_name"
+        cursor.execute(sql)
+        choices = {dict_[attribute] for dict_ in cursor.fetchall()}
+        choices = sorted(choices)
+    
+    return choices
+
 def create_special_query_btns(root):
     def popup_acts_res(act_result):
         win = Toplevel(bg="black")
@@ -47,6 +59,22 @@ def create_special_query_btns(root):
             Label(win, text = result_dict['death_date'], borderwidth = 1,bg="royalblue2").grid(row = i, column = 4)
             Label(win, text = result_dict['state_abbr'], borderwidth = 1,bg="royalblue2").grid(row = i, column = 5)
     
+    def state_result_popup(search_result):
+        win = Toplevel(bg="black")
+        win.wm_title("Search result")
+        win.geometry("1200x800")
+        Label(win, text= 'State Abbreviation', borderwidth = 1, bg="royalblue2").grid(row = 0, column = 0)
+        Label(win, text= 'State Name', borderwidth = 1, bg="royalblue2").grid(row = 0, column = 1)
+        Label(win, text= 'Population 2010', borderwidth = 1, bg="royalblue2").grid(row = 0, column = 2)
+        Label(win, text= 'Population 2019', borderwidth = 1, bg="royalblue2").grid(row = 0, column = 3)
+        i = 0
+        for result_dict in search_result:
+            i += 1
+            Label(win, text = result_dict['state_abbr'], borderwidth = 1, bg="royalblue2").grid(row = i, column = 0)
+            Label(win, text = result_dict['state_name'], borderwidth = 1, bg="royalblue2").grid(row = i, column = 1)
+            Label(win, text = result_dict['population_2010'], borderwidth = 1, bg="royalblue2").grid(row = i, column = 2)
+            Label(win, text = result_dict['population_2019'], borderwidth = 1, bg="royalblue2").grid(row = i, column = 3)
+    
     def write_label(win):
         row = 0
 
@@ -56,6 +84,27 @@ def create_special_query_btns(root):
         pd_name_label.grid(row=row,column=0)
         row+=1
 
+        return pd_name
+    
+    def write_label_state(win):
+        row = 0
+        # gender.set('Unspecified') # set the default option
+        # gender_dropdown = OptionMenu(win, gender, *dropdown_choices("gender"))
+        # gender_dropdown.grid(row = row, column =1)
+        # pd_name = Entry(win,width=30,bg="royalblue2")
+        # pd_name.grid(row=row,column=1)
+
+        states = {'Alabama'}
+
+        pd_name = StringVar(root)
+        pd_name.set('Unspecified')
+        pd_name_dropdown = OptionMenu(win, pd_name, *dropdown_choices("state_abbr"))
+        pd_name_dropdown.grid(row = row, column=1)
+        pd_name_label = Label(win, text="Name of State",bg="royalblue2")
+        pd_name_label.grid(row=row,column=0)
+        row+=1
+
+        print(pd_name)
         return pd_name
 
     def pop_up_acts():
@@ -114,13 +163,29 @@ def create_special_query_btns(root):
             
     def pop_up_victims():
         win = Toplevel(bg="black")
-        win.wm_title("Find Names of those killed by Specific Police Departments")
+        win.wm_title("Find information on States")
         win.geometry("600x200")
 
-        pd_name = write_label(win)
+        pd_name = write_label_state(win)
         def search():
+            pd_text = pd_name.get()
+
+            try:
+                connection = pymysql.connect(credentials.host,credentials.username,credentials.password,credentials.db_name,
+                cursorclass=pymysql.cursors.DictCursor)
+                with connection.cursor() as cursor:
+                    sql = "select * from state where state_abbr like '%" + pd_text + "%';"
+                    cursor.execute(sql)
+                    state_result = cursor.fetchall()
+                    print(state_result)
+                    connection.commit()
+            finally:
+                connection.close()
+                #pd_name.delete(0,END)
+                state_result_popup(state_result)
+
             #clears the text from the boxes
-            pd_name.delete(0, END)
+            #pd_name.delete(0, END)
         
         a = Button(win, text="Search Database", command=search,highlightbackground="royalblue2")
         a.grid(row=2, column=0)
@@ -137,7 +202,7 @@ def create_special_query_btns(root):
     
     row+=1
         
-    delete_btn = Button(root, text="Find Names of those killed by Specific Police Departments", 
+    delete_btn = Button(root, text="Find information on States", 
     command=pop_up_victims,highlightbackground="royalblue2")
     delete_btn.grid(row=row,column=2,columnspan=2, pady=20,padx=10,ipadx=50)
     
